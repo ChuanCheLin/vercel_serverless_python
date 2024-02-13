21b0b8727d390da0e592ca239d0aed5a8c4599ee
ChuanCheChe <diamond16888@gmail.com>
2024-02-13 00:45:13 -0600

    fix the CORS (Cross-Origin Resource Sharing) issue

--- api/generate_puzzle.py
+++ api/generate_puzzle.py
@@ -7,6 +7,12 @@ import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        # List of allowed origins
+        allowed_origins = ['https://portfolio-eric-lin.vercel.app', 'http://localhost:3000']
+
+        # Get the origin of the incoming request
+        origin = self.headers.get('Origin')
+
         query_components = parse_qs(urlparse(self.path).query)
         height = int(query_components.get('height', [5])[0])
         width = int(query_components.get('width', [5])[0])
@@ -15,5 +21,10 @@ class handler(BaseHTTPRequestHandler):
 
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
+
+        # Check if the origin is in our list of allowed origins
+        if origin in allowed_origins:
+            self.send_header('Access-Control-Allow-Origin', origin)
+
         self.end_headers()
         self.wfile.write(json.dumps(puzzle_data).encode())
